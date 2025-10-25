126a26b4425d62687e021424f8271c5d4a7c7b0c
Otavio27-a <oteivious27@gmail.com>
2025-10-24 21:06:35 -0300

    comandos executados

---
+++ AcousticPulse.API/Models/Produto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcousticPulse.API.Models
+{
+    public class Produto
+    {
+        
+    }
+}
